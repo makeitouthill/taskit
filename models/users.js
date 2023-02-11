abbf276eff01e5b0051491a5df23b0eab0b936a6
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-11 11:26:22 -0800

    coded users model

---
+++ models/users.js
@@ -0,0 +1,67 @@
+const { Model, DataTypes } = require ('sequelize');
+const bcrypt = require ('bcrypt');
+const sequelize = require ('../config/connection');
+const { Hooks } = require('sequelize/types/hooks');
+
+class User extends Model{
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        first_name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate : {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+        view_preference: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    },   
+{
+    hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        beforeUpdate: async (updateUserData) => {
+            updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+            return updateUserData;
+        },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  
+}
+);
+module.exports = User;
