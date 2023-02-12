61bbadcc21dc3940329f2267e91ebc002f1fbbf4
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2023-02-11 22:13:35 -0800

    Reformat Model JobOffer

--- models/jobOffer.js
+++ models/jobOffer.js
@@ -1,40 +1,49 @@
-const Sequelize = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class JobOffer extends Sequelize.Model {}
-JobOffer.init({
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  user_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  service_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  offer_price: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  }
-}, {
+class JobOffer extends Model {}
+
+JobOffer.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        service_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        offer_price: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        }
+    }, 
+{
   sequelize,
   modelName: 'job_offer'
-});
+}
+);
 
-sequelize.sync({force: false})
-  .then(() => {
-    console.log(`JobOffer table has been successfully recreated`);
-})
-  .catch(error => {
-    console.error('Error creating JobOffer table: ', error);
-});
+sequelize.sync(
+        {force: false}
+        )
+        .then(() => {
+            console.log(`JobOffer table has been successfully created`);
+        }
+        )
+        .catch(error => {
+            console.error('Error creating JobOffer table: ', error);
+        }
+        );
 
 module.exports = JobOffer;
