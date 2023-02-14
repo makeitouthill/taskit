46ada3918fa8755e524a722c463e081f3b0b3d7e
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-02-13 21:44:53 -0800

    create serviceRange seed data

---
+++ seeds/serviceRange.js
@@ -0,0 +1,16 @@
+const { ServiceRange } = require('../models');
+
+const serviceRangeData = [
+    {
+        service_provider_id: 1,
+        range_miles: 9.5
+    },
+    {
+        service_provider_id: 2,
+        range_miles: 12
+    },
+    {
+        service_provider_id: 3,
+        range_miles: 5
+    }
+];
