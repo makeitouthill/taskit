46e542ca0d396ac7fbdda67c2f3fccfe46906c53
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-02-14 02:48:22 -0800

    update server to change order and allow proper logging in

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -26,6 +26,7 @@ router.get('/login', (req, res) => {
     res.render('login');
   });
 
+// Sign up
 router.get('/signup', (req, res) => {
     // if (req.session.loggedIn) {
     //   res.redirect('/');
@@ -33,5 +34,41 @@ router.get('/signup', (req, res) => {
     // }
     res.render('signup');
 });
+
+// Login post
+router.post('/login', async (req, res) => {
+    // try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+    
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+    
+        // Create session variables based on the logged in user
+        req.session.save(() => {
+        req.session.userId = userData.id;
+        req.session.loggedIn = true;
+        req.session.viewPreference = userData.view_preference;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    // } catch (err) {
+    //   res.status(400).json(err);
+    // }
+  });
+  
 
   module.exports = router;
