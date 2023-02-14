8fbab8c42b7d1c609cd4fc45ae3bbe7b7dfbb630
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-02-13 20:41:19 -0800

    minor updates to model files and removed jobLocations file

--- models/index.js
+++ models/index.js
@@ -1,98 +1,101 @@
-const Job  = require('./job');
-const Service   = require('./jobLocation');
+const Job = require('./job');
 const JobOffer = require('./jobOffer');
+const JobStatusName = require('./jobStatusName');
 const Location = require('./location');
 const Profile = require('./profile');
 const Service = require('./service');
 const ServiceRange = require('./serviceRange');
-const JobStatusName= require('./jobStatusName');
-const User = require('./user')
+const User = require('./user');
 
 
 User.hasMany(JobOffer, {
   foreignKey:'user_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
 });
 
 JobOffer.hasOne(User, {
   foreignKey:'user_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
 });
 
 User.hasMany(Job, {
   foreignKey: 'customer_id',
-  foreignKey: 'service_provider_id',
+  foreignKey: 'service_provider_id'
 });
 
 Job.hasOne(User, {
   foreignKey: 'customer_id',
-  foreignKey: 'service_provider_id',
+  foreignKey: 'service_provider_id'
 });
 
 User.hasOne(ServiceRange, {
   foreignKey: 'service_provider_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
 });
 
 ServiceRange.hasOne(User,{
   foreignKey: 'service_provider_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
 });
 
 Profile.hasOne(User,{
   foreignKey:'user_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
 });
 
 User.hasMany(Profile, {
   foreignKey:'user_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Location, {
+  foreignKey: 'user_id'
+});
+
+Location.hasMany(User, {
+  foreignKey: 'user_id'
 });
 
 Location.hasOne(Profile,{
-  foreignKey: 'location_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'location_id'
 });
 
 Profile.hasOne(Location,{
-  foreignKey: 'location_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'location_id'
 });
 
 JobOffer.hasOne(Service, {
-  foreignKey:'service_id',
+  foreignKey:'service_id'
 });
 
 Service.hasMany(JobOffer,{
-  foreignKey:'service_id',
+  foreignKey:'service_id'
 });
 
-Job.hasOne(Service,{
-  foreignKey: 'service_id',
-  
+Job.hasOne(Service, {
+  foreignKey: 'service_id'
 });
 
-Service.hasMany(Job,{
-  foreignKey: 'service_id',
- 
+Service.hasMany(Job, {
+  foreignKey: 'service_id'
 });
 
-Job.hasOne(JobStatusName,{
-  foreignKey: 'job_id',
+Job.hasOne(JobStatusName, {
+  foreignKey: 'status_id'
 });
 
-JobStatusName.hasOne(JobStatusName,{
-  foreignKey: 'status_id',
-  
+JobStatusName.hasOne(JobStatusName, {
+  foreignKey: 'status_id'
 });
+
 // update location model
-Job.hasOne(Location,{
-  foreignKey: 'location_id',
+Job.hasOne(Location, {
+  foreignKey: 'location_id'
 });
 
 //update location model
-Location.hasMany(Job,{
-  ForeignKey: 'location_id',
+Location.hasMany(Job, {
+  foreignKey: 'location_id'
 });
 
-module.exports = { Job, JobLocation, JobOffer, Location, Profile, ServiceRange, Service,  User, JobStatusName};
+module.exports = { Job, JobOffer, JobStatusName, Location, Profile, Service, ServiceRange, User };
